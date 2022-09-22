27054d99eb32d9fc82d3437633178072c62bda99
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2022-09-22 09:16:31 -0300

    nodeexpress

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -7,6 +7,53 @@ class LivroController {
             res.status(200).json(livros)
         }) 
     }
+
+    static cadastrarLivros = (req,res) => {
+        let livro = new livros(req.body);
+
+        livro.save((err) => {
+            if(err) {
+                res.status(500).send({message: `${err.message} - falha ao cadastrar`})
+            }else{
+                res.status(201).send(livro.toJSON())
+            }
+        })
+    }
+
+    static atualizarLivro = (req,res) => {
+        const id = req.params.id;
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({message: 'Livro atualizado'})
+            }else{
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+
+    static listarLivrosId = (req,res) => {
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros) => {
+            if(err){
+                res.status(400).send({message: `${err.message} - Id não localizada`})
+            }else{
+                res.status(200).send(livros);
+            }
+        })
+    }
+
+    static excluirLivro = (req, res) => {
+        const id = req.params.id
+
+        livros.findByIdAndDelete(id, (err) => {
+            if(!err){
+                res.status(200).send({message: 'Livro removido'})
+            }else{
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
 }
 
 export default LivroController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 router
     .get("/livros", LivroController.listarLivros)
+    .get("/livros/:id", LivroController.listarLivrosId)
+    .post("/livros", LivroController.cadastrarLivros)
+    .put("/livros/:id", LivroController.atualizarLivro)
+    .delete("/livros/:id", LivroController.excluirLivro)
 
 export default router;
